601438405e18fc9fd1ef1b6fa67a6687b37feab5
qianzhangl <yujianhu@zhangqiandeMacBook-Pro.local>
2016-12-25 16:51:54 +0800

    add a DemoView

---
+++ Demo/Demo/DemoView.h
@@ -0,0 +1,13 @@
+//
+//  DemoView.h
+//  Demo
+//
+//  Created by 张倩 on 16/12/25.
+//  Copyright © 2016年 QiaoLian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DemoView : UIView
+
+@end
---
+++ Demo/Demo/DemoView.m
@@ -0,0 +1,21 @@
+//
+//  DemoView.m
+//  Demo
+//
+//  Created by 张倩 on 16/12/25.
+//  Copyright © 2016年 QiaoLian. All rights reserved.
+//
+
+#import "DemoView.h"
+
+@implementation DemoView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
